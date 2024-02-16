c08832dfd4f32d0e6fa4ab972c0e28967db9143f
JoemariePortacio <joemarieportacio00@gmail.com>
2024-02-16 08:23:35 +0800

    Wala

--- resources/views/layouts/system.blade.php
+++ resources/views/layouts/system.blade.php
@@ -11,6 +11,7 @@
                 <button ng-click="mobileMenu = !mobileMenu" type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                     <span class="absolute -inset-0.5"></span>
                     <span class="sr-only">Open main menu</span>
+                    {{-- ajsdhjkahdjkas --}}
                     <!--
                     Icon when menu is closed.
         
